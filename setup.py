8dcde21b3d24284dcfe949a06a2bf2ff4fd67e95
Issem-Sato <the.filip.juren@gmail.com>
2024-07-09 10:18:44 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='modulo_nobattery',  
-    version='0.1', 
+    name='modulo_nobattery',  # Nome del pacchetto
+    version='0.1',  # Versione del pacchetto
     packages=find_packages(),
-    install_requires=[],
-    author='Agenzia APE',
+    install_requires=[],  # Elenca qui le dipendenze del tuo pacchetto, se ce ne sono
+    author='Il tuo nome',
+    author_email='tuo.email@example.com',
+    description='Una breve descrizione del pacchetto',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Issem-Sato/modulo_nobattery',
